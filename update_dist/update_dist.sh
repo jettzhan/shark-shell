183241123cd117c4a28e2620e86d0c037197722a
zhanzhihu <noodzhan@163.com>
2025-01-16 15:55:38 +0800

    feat: 提交升级前端

---
+++ update_dist/update_dist.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+# author: zhanzhihu
+set -e 
+
+des="/opt/apexapp/web"
+
+filename=dist$(date +"%Y%m%d%s")
+sudo cp -r $des/dist $des/$filename
+ls -clt $des |head -n 5
+
+cur=$(pwd)
+
+exist=$(ls -alct $cur | grep dist.zip |wc -l) 
+
+if [ $exist -eq 1 ] ;then
+	sudo cp $cur/dist.zip $des
+  sudo unzip -o $des/dist.zip -d $des
+	sudo cp $des/$filename/static/config.js $des/dist/static/
+	echo "update ok "	
+else
+   echo 'this path not found dist.zip'
+
+fi
+
