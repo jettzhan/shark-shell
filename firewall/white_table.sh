1ba6f3693639144e0cb13b25282949fc3a50fdc0
zhanzhihu <noodzhan@163.com>
2025-01-22 11:27:59 +0800

    feat: 提交代码

--- firewall/white_table.sh
+++ firewall/white_table.sh
@@ -9,7 +9,9 @@ allowed_ips=("172.24.20.12" "172.24.20.13")
 # 拒绝访问的端口
 unallowed_ports=("3306" "5432" "7070")
 
-
+#sudo iptables -A INPUT -s 10.124.160.0/24 -p tcp --dport 22 -j REJECT
+#sudo iptables -A INPUT -s 10.124.160.0/24 -p tcp --dport 80 -j ACCEPT
+#sudo iptables -A INPUT -s 10.124.160.0/24 -p tcp -j REJECT
 
 for ip in "${allowed_ips[@]}"; do
   sudo iptables -A INPUT -s "$ip" -j ACCEPT
