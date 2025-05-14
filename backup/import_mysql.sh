273424f3bc4c5319aa60a1aa333fb78c93fa9b8d
zhanzhihu <noodzhan@163.com>
2025-05-14 10:31:52 +0800

    feat: 支持导入数据

---
+++ backup/import_mysql.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BAKDIR="/home/noodzhan/mysql"
+DBUSER=root
+DBPWD=123456
+HOST="127.0.0.1"
+
+echo 'import mysql'
+
+for file in $(ls ${BAKDIR}/*.sql); do
+  # 获取数据库名称
+  DBNAME=$(basename $file | cut -d'2' -f1 | rev | cut -c2- | rev)
+
+  echo "Importing database: ${DBNAME}"
+
+  echo $file
+
+  mysql -h${HOST} -u${DBUSER} -p${DBPWD} -e "DROP DATABASE IF EXISTS ${DBNAME};"
+
+  # 创建新数据库
+  mysql -h${HOST} -u${DBUSER} -p${DBPWD} -e "CREATE DATABASE ${DBNAME};"
+
+  mysql -h${HOST} -u${DBUSER} -p${DBPWD} ${DBNAME} <$file
+
+done
+
+echo 'MySQL import completed'
