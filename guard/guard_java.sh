af1f7d4da25cde87f9f9bad95a10240b3a9856fa
noodzhan <noodzhan@163.com>
2025-07-02 23:14:07 +0800

    fix: 修改命名问题

--- guard/guard_java.sh
+++ guard/guard_java.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # 配置端口和对应的启动命令
-ports=("3306")
+ports=("8081" "8099")
 commands=(
-    "sudo systemctl start mysql"
+    "nohup java -jar /opt/nbcio/nbcio-boot-module-system-3.1.jar > /dev/null 2>&1 &"
+    "nohup java -jar -Dspring.profiles.active=prod /opt/service/taos/jeecg-system-start-3.4.4.jar > /dev/null 2>&1 &"
 )
 
 # 检查端口是否在监听状态
--- guard/guard_mysql.sh
+++ guard/guard_mysql.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # 配置端口和对应的启动命令
-ports=("8081" "8099")
+ports=("3306")
 commands=(
-    "nohup java -jar /opt/nbcio/nbcio-boot-module-system-3.1.jar > /dev/null 2>&1 &"
-    "nohup java -jar -Dspring.profiles.active=prod /opt/service/taos/jeecg-system-start-3.4.4.jar > /dev/null 2>&1 &"
+    "sudo systemctl start mysql"
 )
 
 # 检查端口是否在监听状态
